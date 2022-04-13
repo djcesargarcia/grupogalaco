9230ce651592b80651d267e9a33cc455f6561584
djcesargarcia <djcesargarcia@gmail.com>
2022-04-13 21:38:44 +0100

    Adding news path and directories

--- transport/urls.py
+++ transport/urls.py
@@ -2,8 +2,15 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index.html'),
-  
+    path('',views.index, name='index'),
+    path('drivers', views.drivers, name='drivers'),
+    path('drivers', views.create, name='create'),
+    path('drivers', views.edit, name='edit'),
+    path('home', views.home, name='home'),
+    path('routes', views.routes, name="routes"),
+    path('routes', views.create, name="create"),
+    path('routes', views.edit, name="edit"),
+    path('customers', views.customers, name="customers")
 ]
 
 
--- transport/views.py
+++ transport/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
-from . import views
+from django.http import HttpResponse
+
+#Create your views here.
+
+#Path App.
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request,'transport/index.html')
+
+#Path Drivers.
+
+def drivers(request):
+    return render(request, 'drivers/index.html')
+
+def create(request):
+    return render(request,'drivers/crear.html')
+
+def edit(request):
+    return render(request, 'drivers/editar')
+
+#Path for Home.
+
+def home(request):
+    return render(request, 'home/index.html')
+
+#Path for Routes.
+
+def routes(request):
+    return render(request, 'routes/index.html')
+
+#Path for Customers.
+
+def customers(request):
+    return render(request, 'customers/index.html')
+
+#Path for Users.
+
+
 
 
