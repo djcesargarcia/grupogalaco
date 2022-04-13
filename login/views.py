f5dfaa3b3f158ec0698dd9e0c159767c5711e41e
Cesar Garcia <djcesargarcia@gmail.com>
2022-04-13 11:15:01 +0100

    Creating templates for our login

--- login/views.py
+++ login/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authentication
+from django.contrib import messages
 
-# Create your views here.
+
+
+
--- tmsproject/urls.py
+++ tmsproject/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('transport.urls')),
+    path('login/', include('login.urls')),
+    path('login/', include('django.contrib.auth.urls'))
 ]
