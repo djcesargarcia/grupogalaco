69e73fdf581e29642a16c72716918ec9e0e623e8
Cesar Garcia <djcesargarcia@gmail.com>
2022-04-18 14:49:47 +0100

    Deleted file init.py

--- routes/tests.py
+++ routes/tests.py
@@ -3,9 +3,11 @@ from routes.models import Routes
 
 # Create your tests here.
 
-class RoutesTest(TestCase):
+class RouteTest(TestCase):
     def setUp(self):
         route_a = Routes(place="Las Palmas", origin="Mesa y Lopez", destiny="San Mateo", postal_code="35016")
         route_a.save() 
+        
+        
         
         
--- settings.py
+++ settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drivers',
     'tmsapp',
-    'routes'
+    'routes',
 ]
 
 MIDDLEWARE = [
