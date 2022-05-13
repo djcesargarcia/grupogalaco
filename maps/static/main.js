27ecd1a3feea45eb2b21c8605a3e1133fd927067
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-13 10:02:53 +0100

    Modifying my maps

--- maps/static/main.js
+++ maps/static/main.js
@@ -9,22 +9,5 @@ var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
         zoomOffset: -1
 }).addTo(map);
  
-  // Put markers in Leaflet
-var marker = L.marker([28.11, -15.45]).addTo(map)
-  .bindPopup('<b>Estoy en las torres.').openPopup();
-
-  // Put popus in Leaflet
-var popup = L.popup()
-.setLatLng([28.11, -15.45])
-.setContent('I am a standalone popup.')
-.openOn(map);
-
-function onMapClick(e) {
-  popup
-      .setLatLng(e.latlng)
-      .setContent('You clicked the map at ' + e.latlng.toString())
-      .openOn(map);
-}
-
 L.control.locate().addTo(map);
 loadMap('map');
