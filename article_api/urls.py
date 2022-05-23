7705aae86b855c7d414f60e26950fcefadd10400
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-23 09:45:13 +0100

    Adding routers in urls article api

--- article_api/urls.py
+++ article_api/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'orders', views.ArticleViewSet)
+router.register(r'articles', views.ArticleViewSet)
 
 
 # Wire up our API using automatic URL routing.
