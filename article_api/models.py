cb947dbdb9045dd08136d45f073631de61919d0d
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-23 09:29:06 +0100

    Modifying Article API to our project

--- article_api/models.py
+++ article_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from rest_framework import serializers
 
 # Create your models here.
+
+class ArticleSerializers(serializers.ArticleSerializers):
+    name = serializers.CharField(max_length=50)
+    weigth = serializers.CharField(max_length=5)
+    image = serializers.ImageField()
+    price = serializers.DecimalField(max_digits=4, decimal_places=2)
+    
+    
+    
+    
