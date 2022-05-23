7d06c6cf376fd2cade0d4a825f66499ee64bfae7
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-23 09:59:34 +0100

    Adding to article api new things

--- article_api/serializers.py
+++ article_api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 # Create your models here.
 
-class ArticleSerializers(serializers.ArticleSerializers):
+class ArticleSerializers(serializers.Serializer):
     name = serializers.CharField(max_length=50)
     weigth = serializers.CharField(max_length=5)
     image = serializers.ImageField()
