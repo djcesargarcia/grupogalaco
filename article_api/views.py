15c829adddd9b28e31f6af59eed44cc378ccaf9a
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-23 09:39:51 +0100

    Adding modyfiers to our article api

--- article_api/views.py
+++ article_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from article.models import Article
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ArticleSerializers
 
 # Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows orders to be viewed or edited
+    """
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializers
+    permission_classes = [permissions.IsAuthenticated]
+    
+
