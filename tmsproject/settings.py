721a7a1b12233fa1641c0bd05d2d40cbdfaf15d4
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-25 13:14:10 +0100

    Adding templates migrations for vehicle

--- tmsproject/settings.py
+++ tmsproject/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'maps',
     'provider',
     'driver_api',
+    'vehicle',
 ]
 
 MIDDLEWARE = [
--- vehicle/models.py
+++ vehicle/models.py
@@ -6,12 +6,12 @@ from django.utils.translation import gettext
 
 # Create your models here.
 
-class Vehicule(models.Model):
+class Vehicle(models.Model):
     id = models.AutoField(primary_key=True, serialize=False)
     brand = models.CharField(max_length=50, verbose_name='Brand')
     model = models.CharField(max_length=50, verbose_name='Model')
     plate = models.CharField(max_length=9, verbose_name='Plate')
-    wheels = models.IntegerField(max_digits=1, verbose_name='Wheels')
+    wheels = models.IntegerField(max_length=1, verbose_name='Wheels')
     image = models.ImageField(upload_to='images/', verbose_name='Imagen' ,null=True)
   
     def __str__(self):
