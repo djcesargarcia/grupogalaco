5fecb8c08b073630186dd0c3c8ddd83cf8b46871
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-26 09:34:57 +0100

    Vehicle API is OK

--- vehicle_api/views.py
+++ vehicle_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from vehicle.models import Vehicle
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import VehicleSerializer
 
-# Create your views here.
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    permission_classes = [permissions.IsAuthenticated]
