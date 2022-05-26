f698cd2f680bb7c9392ec230272fd0ab123d8b27
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-26 09:31:35 +0100

    Vehicle serializers is OK

---
+++ vehicle_api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from article.models import Article
+from vehicle.models import Vehicle
+from django.contrib.auth.models import models
+
+class VehicleSerializer(serializers.Serializer):
+    
+    id = serializers.IntegerField(read_only=True)
+    brand = serializers.CharField(max_length=50)
+    model = serializers.CharField(max_length=50)
+    plate = serializers.CharField(max_length=9)
+    wheels = serializers.IntegerField()
+    image = serializers.ImageField()
+    
+    def create(self,validated_data):
+        return Vehicle.objects.create(**validated_data)
+    
+    
