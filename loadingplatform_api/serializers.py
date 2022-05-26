137c0d80bb2ba4eb54f628361a6f2d6cbcbc2ca7
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-26 09:01:46 +0100

    LoadingPlatorm views and serializers is OK

--- loadingplatform_api/serializers.py
+++ loadingplatform_api/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
-from driver.models import Driver
-from routes.models import Routes
+from loadingplatform.models import LoadingPlatform
 from django.contrib.auth.models import models
 
-class DriverSerializer(serializers.Serializer):
+class LoadingPlatformSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=50)
     position = serializers.IntegerField()
     image = serializers.ImageField()
     
     def create(self,validated_data):
-        return Driver.objects.create(**validated_data)
+        return LoadingPlatform.objects.create(**validated_data)
 
     
     
--- loadingplatform_api/views.py
+++ loadingplatform_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from loadingplatform.models import LoadingPlatform
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import LoadingPlatformSerializer
 
-# Create your views here.
+
+class DriverViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows drivers to be viewed or edited.
+    """
+    queryset = LoadingPlatform.objects.all()
+    serializer_class = LoadingPlatformSerializer
+    permission_classes = [permissions.IsAuthenticated]
