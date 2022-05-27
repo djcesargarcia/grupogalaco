0ff4fea8443ab18f5d51cb61cde4f5f65ad3d0da
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-27 10:39:13 +0100

    Adding Models Routes Diaries

---
+++ routesdiaries/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoutesdiariesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'routesdiaries'
---
+++ routesdiaries_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoutesdiariesApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'routesdiaries_api'
