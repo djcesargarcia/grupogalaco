df548dfb34bc62595262dc734db8124fff460d60
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-27 08:51:14 +0100

    Dock Api is ready!

--- dock_api/views.py
+++ dock_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from customer.models import Customer
+from rest_framework import viewsets
+from rest_framework import permissions
+from dock.models import Dock
+from .serializers import DockSerializer
 
 # Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows orders to be viewed or edited
+    """
+    queryset = Dock.objects.all()
+    serializer_class = DockSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+
