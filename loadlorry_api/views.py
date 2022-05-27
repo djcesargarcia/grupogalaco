fa3696db5b5effee98e63424e8ef6df067d3ac79
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-27 13:26:54 +0100

    Load Lorry API views is OK

--- loadlorry_api/views.py
+++ loadlorry_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from customer.models import Customer
+from rest_framework import viewsets
+from rest_framework import permissions
+from loadlorry.models import LoadLorry
+from .serializers import LoadLorrySerializer
 
 # Create your views here.
+
+class DockViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows orders to be viewed or edited
+    """
+    queryset = LoadLorry.objects.all()
+    serializer_class = LoadLorrySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+
