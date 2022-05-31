d73c9452020f89e8295d186ca692a8db6b30e8f7
Prácticas IT <pit@AD.GALACO.LAN>
2022-05-31 09:11:24 +0100

    Adding type of data floatfield in loadlorry models

--- loadlorry/models.py
+++ loadlorry/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from numpy import maximum
 
 # Create your models here.
 
 class LoadLorry(models.Model):
     id = models.AutoField(primary_key=True, verbose_name="ID")
-    weight = models.FloatField(verbose_name="Weight")
-    volume = models.FloatField(verbose_name="Volume")
-    mma = models.FloatField(verbose_name="MMA")
-    tare = models.FloatField(verbose_name="Tare")
-    cuantity = models.FloatField(verbose_name="Cuantity")
-    empty_weight = models.FloatField(verbose_name="Empty Weight")
-    total_weight = models.FloatField(verbose_name="Total Weight")
+    weight = models.DecimalField(max_digits=5, decimal_places=3)
+    volume = models.DecimalField(max_digits=5, decimal_places=3)
+    mma = models.DecimalField(max_digits=5, decimal_places=3)
+    tare = models.DecimalField(max_digits=5, decimal_places=3)
+    cuantity = models.DecimalField(max_digits=5, decimal_places=3)
+    empty_weight = models.DecimalField(max_digits=5, decimal_places=3)
+    total_weight = models.DecimalField(max_digits=5, decimal_places=3)
     
     def __str__(self):
         fila = "Id: " + self.id + " - " + "Weight: " + self.weight + " - " + "Volume: " + self.volume + " - " + "MMA: " + self.mma + " - " + "Cuantity: " + self.cuantity + " - " + "Tare: " + self.tare + " - " + "Empty Weight: " + self.empty_weight + " - " + "Total Weight: " + self.total_weight
